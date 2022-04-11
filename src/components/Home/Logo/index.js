cfd688d6636cc049ec771a1999b14db57db539f1
Junmo <tauche.t3473@gmail.com>
2022-04-11 23:18:15 +0900

    strictMode 해제

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -9,26 +9,26 @@ const Logo = () => {
   const outlineLogoRef = useRef();
   const solidLogoRef = useRef();
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+  // useEffect(() => {
+  //   gsap.registerPlugin(DrawSVGPlugin)
 
-    gsap.timeline().to(bgRef.current, {
-      duration: 1,
-      opacity: 1
-    })
-    .from(outlineLogoRef.current, {
-      drawSVG: 0,
-      duration: 20,
-    })
+  //   gsap.timeline().to(bgRef.current, {
+  //     duration: 1,
+  //     opacity: 1
+  //   })
+  //   .from(outlineLogoRef.current, {
+  //     drawSVG: 0,
+  //     duration: 20,
+  //   })
 
-    gsap.fromTo(solidLogoRef.current, {
-      opacity: 0,
-    }, {
-      opacity: 1,
-      delay: 4,
-      duration: 2,
-    })
-  }, []);
+  //   gsap.fromTo(solidLogoRef.current, {
+  //     opacity: 0,
+  //   }, {
+  //     opacity: 1,
+  //     delay: 4,
+  //     duration: 2,
+  //   })
+  // }, []);
 
   return (
     <div className="logo-container" ref={bgRef}>
