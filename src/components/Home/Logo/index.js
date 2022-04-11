56462f742aae7afec16d86fb4ed155f422dde7dd
Junmo <tauche.t3473@gmail.com>
2022-04-11 23:29:31 +0900

    DrawSVG 삭제..

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,34 +1,34 @@
 import './index.scss';
 import LogoJ from '../../../assets/images/logo-j.png';
 import { useEffect, useRef } from 'react';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import gsap from 'gsap-trial';
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
+// import gsap from 'gsap-trial';
 
 const Logo = () => {
   const bgRef = useRef();
   const outlineLogoRef = useRef();
   const solidLogoRef = useRef();
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+  // useEffect(() => {
+  //   gsap.registerPlugin(DrawSVGPlugin)
 
-    gsap.timeline().to(bgRef.current, {
-      duration: 1,
-      opacity: 1
-    })
-    .from(outlineLogoRef.current, {
-      drawSVG: 0,
-      duration: 20,
-    })
+  //   gsap.timeline().to(bgRef.current, {
+  //     duration: 1,
+  //     opacity: 1
+  //   })
+  //   .from(outlineLogoRef.current, {
+  //     drawSVG: 0,
+  //     duration: 20,
+  //   })
 
-    gsap.fromTo(solidLogoRef.current, {
-      opacity: 0,
-    }, {
-      opacity: 1,
-      delay: 4,
-      duration: 2,
-    })
-  }, []);
+  //   gsap.fromTo(solidLogoRef.current, {
+  //     opacity: 0,
+  //   }, {
+  //     opacity: 1,
+  //     delay: 4,
+  //     duration: 2,
+  //   })
+  // }, []);
 
   return (
     <div className="logo-container" ref={bgRef}>
